6f0bb849479bc23b816bb334c9bceba5c975830a
Superbility <75504654+Superbility@users.noreply.github.com>
2022-10-21 12:19:37 +0100

    Delete src/main directory

--- src/main/java/com/superdevelopment/survivalcore/data/items/builder/data/Selected.java
+++
@@ -1,5 +0,0 @@
-package com.superdevelopment.survivalcore.data.items.builder.data;
-
-public enum Selected {
-    NONE, NAME, USES, HEALTH, DEFENSE, INTELLIGENCE, FORCE, CRITDAMAGE, LUCKYCHANCE, SPEED, AGILITY, RARITY, ABILITY, BASEDAMAGE;
-}
